900821c1ef3f2f1e709718775e9f605d15903c0e
jxtrbtk <j.t@free.fr>
2020-11-11 17:32:26 +0100

    operator QS: adding low fund invest trigger

--- operatorQS.py
+++ operatorQS.py
@@ -12,7 +12,8 @@ from decimal import Decimal
 import lib
 
 RATIO = 1/3
-INVEST_TRG = 0.99
+INVEST_TRG_HIGH = 0.99
+INVEST_TRG_LOW = 0.01
 
 # sudo docker run -it -v secret:/secret operator:vQS.0
 
@@ -263,7 +264,7 @@ def operation_loop(df):
         else: 
             print("{}| conditions not met for {} fork action            ".format(ts, t_data["pair"]))
 
-            if (t_data["investQuote"] > INVEST_TRG and  t_data["investQuote"] > t_data["investBase"]) or t_data["investBase"] == 0.0: 
+            if (t_data["investQuote"] > INVEST_TRG_HIGH and  t_data["investQuote"] > t_data["investBase"]) or t_data["investBase"] <= INVEST_TRG_LOW: 
                 mode = "Invest (quote)"
                 qty_sell = 0.05  * t_data["share_BNB"] / t_data["priceBase_BNB"]
                 if qty_sell < float(t_data["lot_size"]): qty_sell = float(t_data["lot_size"])
@@ -276,7 +277,7 @@ def operation_loop(df):
                     res = lib.wallet.send_order("sell", qty_sell, price_sell, t_data["pair"])
                     print(len(res))
 
-            if (t_data["investBase"] > INVEST_TRG and t_data["investBase"] > t_data["investQuote"]) or t_data["investQuote"] == 0.0:
+            if (t_data["investBase"] > INVEST_TRG_HIGH and t_data["investBase"] > t_data["investQuote"]) or t_data["investQuote"] <= INVEST_TRG_LOW:
                 mode = "Invest (token)"
                 qty_buy = 0.05  * t_data["share_BNB"] / t_data["priceBase_BNB"]
                 if qty_buy < float(t_data["lot_size"]): qty_buy = float(t_data["lot_size"])
