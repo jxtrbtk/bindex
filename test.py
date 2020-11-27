f40a7d97526ad363a6419dfc9f7c387e6194a515
jxtrbtk <j.t@free.fr>
2020-11-27 23:26:39 +0100

    Add test script to prepare xgb container

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import random
+import json
+
+import numpy as np
+import pandas as pd
+
+# import xgboost as xgb
+
+import lib
+import lib.api
+import lib.market
+import lib.wallet
+
+import xgboost
+
+
